112fac47d888afac5fe2ceb2231062c6f3daf3b4
Julia Kwiecien <julia.kwiecien@hatimeria.com>
2022-03-01 10:50:20 +0100

    Fix mock response

--- src/components/Nameday.js
+++ src/components/Nameday.js
@@ -19,7 +19,6 @@ const Nameday = ({ name }) => {
       setError(true);
     }
   }
-  console.log(nameDays);
 
   return (
     <div>
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -3,10 +3,13 @@ import { rest } from 'msw';
 export const handlers = [
   rest.get('https://nameday.abalin.net/api/V1/getname', (req, res, ctx) => {
     return res(
-      ctx.json([
-        {day: 1, month: 10, name: 'Danuta, Remigiusz, Cieszysław, Dan, Danisz, Danuta, Igor, Jan, Remigiusz'},
-        {day: 5, month: 10, name: 'Częstogniew, Donata, Igor, Justyn, Konstancjusz, Rabby'}
-      ])
+      ctx.json({
+        0: [
+          {day: 1, month: 10, name: 'Danuta, Remigiusz, Cieszysław, Dan, Danisz, Danuta, Igor, Jan, Remigiusz'},
+          {day: 5, month: 10, name: 'Częstogniew, Donata, Igor, Justyn, Konstancjusz, Rabby'}
+        ],
+        country: "pl"
+      })
     );
-  }),
-];
+  })
+]
